00cac8e140a956086159dad795e6653008816e47
Lucas Vergara <lucasvergara1@gmail.com>
2020-04-16 14:42:19 -0400

    added "new" and "edit" forms

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+    
+    def index
+        @users = User.all 
+    end
+   
+    def new
+        @user = User.new
+    end
+
+    def show 
+        @user = @user.find(user_params[:id])
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to new_user_path
+        else 
+            render :new
+        end
+    end
+
+    def edit
+        @user = User.find(user_params[:id])
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+end
